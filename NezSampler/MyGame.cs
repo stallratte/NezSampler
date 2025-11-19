164bff30eff4735c0463e58b5ca790b540bf5953
stallratte <deen@gmx.de>
2025-11-19 21:32:56 +0100

    effects

--- NezSampler/MyGame.cs
+++ NezSampler/MyGame.cs
@@ -9,21 +9,26 @@ public class MyGame : Core {
 	protected override void Initialize() {
 		base.Initialize();
 		Scene = new Scene1();
+		Scene.AddPostProcessor(new BloomPostProcessor(0));
 	}
 
 	protected override void Update(GameTime gameTime) {
 		base.Update(gameTime);
 
+		Scene nextScene = null;
 		
 		if (Input.IsKeyPressed(Keys.Space)) {
 			if (Scene is Scene1) {
-				Scene = new Scene2();
-			}
+				nextScene = new Scene2(); }
 			else {
-				Scene = new Scene1();
+				nextScene = new Scene1();
 			}
-			
-			
+
+		}
+		
+		if (nextScene != null) {
+			nextScene.AddPostProcessor(new BloomPostProcessor(0));
+			Scene = nextScene;
 		}
 		
 	}
