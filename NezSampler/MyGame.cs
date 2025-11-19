e02567c63318501688118b77d1a15c0ce92e5371
stallratte <deen@gmx.de>
2025-11-19 21:04:01 +0100

    first commit

---
+++ NezSampler/MyGame.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace NezSampler;
+
+public class MyGame : Core {
+
+	protected override void Initialize() {
+		base.Initialize();
+		Scene = new Scene1();
+	}
+
+	protected override void Update(GameTime gameTime) {
+		base.Update(gameTime);
+
+		if (Keyboard.GetState().IsKeyDown(Keys.Space)) {
+			if (Scene is Scene1) {
+				Scene = new Scene2();
+			}
+			else {
+				Scene = new Scene1();
+			}
+			
+			
+		}
+		
+	}
+
+}
---
+++ NezSampler/Scene2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.Sprites;
+
+namespace NezSampler;
+
+public class Scene2 : Scene{
+
+	public override void Initialize() {
+		var texture = Content.LoadTexture("Content/scene2.png");
+		
+		var entity = CreateEntity("textureEntity", new Vector2(Screen.Width / 2, Screen.Height / 2));
+		entity.AddComponent(new SpriteRenderer(texture));
+	}
+
+}
