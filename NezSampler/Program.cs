0275e7d70e23d23eb7688c30e5818158b4adf2dc
stallratte <deen@gmx.de>
2025-11-19 21:11:25 +0100

    monogame edition

--- NezSampler/Program.cs
+++ NezSampler/Program.cs
@@ -5,11 +5,6 @@ public class Program {
 	[STAThread]
 	static void Main(string[] args)
 	{
-		// SDL_GPU (Vulkan, Metal, D3D12)
-		// D3D11
-		// OpenGL
-		Environment.SetEnvironmentVariable("FNA3D_FORCE_DRIVER", "D3D11");
-		
 		using (MyGame g = new MyGame())
 		{
 			g.Run();
